e6d82e72641622a8a479f62947312d5109308bf8
jbkn5040 <joshua.njoroge@student.moringaschool.com>
2025-01-26 22:30:39 +0300

    All the files used in making the Akan Name Generator

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a website that helps one to know one to know their name according to the Akan Name generator which is dependent
+on the day the person was born and the gender of the person.
---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+document.getElementById('birthForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const birthdate = new Date(document.getElementById('birthdate').value);
+    const gender = document.getElementById('gender').value;
+
+
+
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+
+    const dayOfWeek = birthdate.getDay();
+
+
+
+    let akanName;
+    if (gender === 'male') {
+        akanName = maleNames[dayOfWeek];
+    } else {
+        akanName = femaleNames[dayOfWeek];
+    }
+
+
+    const resultMessage = `You were born on a ${getDayName(dayOfWeek)} and your Akan name is ${akanName}.`;
+    document.getElementById('result').textContent = resultMessage;
+});
+
+
+function getDayName(dayIndex) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[dayIndex];
+}
